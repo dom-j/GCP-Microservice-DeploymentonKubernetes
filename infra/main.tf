08bc9424076ae3bad988f9be761cabdcc25ad95b
Dominik Jaro <dominik.jaro@spotlightsportsgroup.com>
2024-05-04 09:45:37 +0100

    commit 41

--- infra/main.tf
+++ infra/main.tf
@@ -40,7 +40,8 @@ resource "google_container_cluster" "primary" {
       enable_autopilot = true 
     master_auth {  
       client_certificate_config {
-      issue_client_certificate = false
+      issue_client_certificate = false # For authentication to Kubernetes cluster using GCP credentials
+      }
     }
       }
     }
